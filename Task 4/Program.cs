99b65cca6b522f194dc9a6920e47dac228c18c21
Tsykalo Roman <u15921@mail.ru>
2022-10-14 16:16:24 +0400

    Закончил 4 ДЗ

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+
+string input = Console.ReadLine();
+
+int N = Convert.ToInt32(input);
+
+int number = 2;
+
+while (number < N + 1)
+{
+  Console.Write(number + "|" );
+  number = number + 2 ; 
+}
+
+
