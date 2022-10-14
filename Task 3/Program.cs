6f26948a488cd94be4841469f9587837d0448277
Tsykalo Roman <u15921@mail.ru>
2022-10-14 15:55:44 +0400

    Закончил 3 ДЗ

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите число чтобы узнать четное или не четное");
+
+string input = Console.ReadLine();
+
+int number = Convert.ToInt32(input);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
