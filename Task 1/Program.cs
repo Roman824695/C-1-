453329fe0e705a235f7ec05b1e6c9d79612f7bdb
Tsykalo Roman <u15921@mail.ru>
2022-10-14 14:34:58 +0400

    Перваая задача сделана

---
+++ Task 1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+string input = Console.ReadLine();
+int a = Convert.ToInt32(input);
+
+Console.WriteLine("Введите второе число: ");
+string input2 = Console.ReadLine();
+int b = Convert.ToInt32(input2);
+
+
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else if (b > a)
+{
+    Console.WriteLine(b);
+}
